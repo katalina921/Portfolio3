361f88009f0e021cdc26546a49b449d3c5002ddd
Katalina  Arce <katalinaarce34@gmail.com>
2015-01-20 09:20:37 -0800

    accordion works

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,8 +1,4 @@
 $(function(){
 	
 	var pp = $('#push').pointPoint();
-	
-	// To destroy it, call the destroy method:
-	// pp.destroyPointPoint(); 	
-	
 });
---
+++ public_html/portfolio.js
@@ -0,0 +1,14 @@
+$("document").ready(function() { 
+     $('#superHuman').accordion({header: "h4"});
+     $(".Carrot").css("color", "blue"); 
+     $(".Rabbit").css("color","gold"); 
+//     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe); 
+//     $('#1').bind('click', alertButtonClick); 
+//     $('#2').bind('click', replaceWHtml); 
+//     $('#2').bind('click', replaceWText); 
+//     $('#ham').bind('click', addAPara); 
+//     $('#cat').bind('click', removeAPara); 
+//     $('#hidelogo').bind('click', hideTheImage);
+//     $('#showlogo').bind('click', showTheImage);  
+     
+ });
